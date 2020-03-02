80f2019e0d086faf6e7bf69f7ba0a5a165f4f26f
Sayan Brahma <47536918+sbrahma0@users.noreply.github.com>
2020-03-02 15:14:55 -0500

    Update Minion_language_translation.py

--- Minion_language_translation.py
+++ Minion_language_translation.py
@@ -1,1 +1,20 @@
+'''
+Minion language translation
+lower case = 97 - 110 - 122
+'''
 
+def solution(x):
+    result = ''
+    for i in x:
+        if ord(i) >= 97 and ord(i) < 110:
+            result = result + chr(122-(ord(i)-97))
+        elif ord(i) >= 110 and ord(i) <= 122:
+            result = result + chr(97+(122-ord(i)))
+        else:
+            result = result + i
+    return result
+
+  # Driver
+string = "?"
+r = solution(string)
+print(r)
