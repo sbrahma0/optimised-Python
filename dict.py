cccafae92eb96188c1648f35c3c12c616ef5fdc5
Sayan Brahma <47536918+sbrahma0@users.noreply.github.com>
2020-03-03 19:33:51 -0500

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,7 @@
+Dctionary (Hash table)
+a = {'a':'1','b':'2','c':'3'}
+# del a['b']  # This does not alter the order of the dictionary
+# a.pop('b')  # This code alters the order of the dictionary
+# a.popitem()   # This delets the last element along with altering the order
+print(a)
+print(type(a))
