f21ac3b5a14a5c2c86d40a6daedd51179c5bc06e
Sayan Brahma <47536918+sbrahma0@users.noreply.github.com>
2020-03-29 21:56:47 -0400

    Create Matrix_to_spiral.py

---
+++ Matrix_to_spiral.py
@@ -0,0 +1,43 @@
+# Printing a Matrix in spiral form
+
+def spiralPrint(m,n,a):
+    k = 0
+    l = 0
+    
+    while k<m and l<n:
+        # From left to right
+        for i in range (l,n):
+            print(a[k][i], end="")
+        k += 1
+        
+        #print('*******')
+        # From top to bottom
+        #print("-------",k,m,l,n)
+        for i in range (k, m):
+            print(a[i][n-1], end="")
+        n -= 1
+        
+        #print("*******")
+        # Right to left
+        if k<m:
+            for i in range(n-1,l-1,-1):
+                print(a[m-1][i],end="")
+            m -= 1
+        #print("-------",k,m,l,n)
+        #print("**********")
+        # Bottom to top
+        
+        if l<n:
+            for i in range(m-1,k-1,-1):
+                print(a[i][l], end="")
+            l += 1
+        #print("-------",k,m,l,n)
+        #break
+    
+# Drivers code
+a = [ [1, 2, 3, 4], 
+      [7, 8, 9, 10], 
+      [13, 14, 15, 16] ,[17, 18, 19, 20]] 
+        
+R = 4; C = 4
+spiralPrint(R, C, a) 
