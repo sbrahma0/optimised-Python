091705b55316e32407aab5189e20e1cffd2c1dee
Sayan Brahma <47536918+sbrahma0@users.noreply.github.com>
2020-04-05 02:41:45 -0400

    Update merge_2_sorted_linked_list.py

--- linkedlist/merge_2_sorted_linked_list.py
+++ linkedlist/merge_2_sorted_linked_list.py
@@ -46,7 +46,48 @@ def merge(head1, head2):
         current.nest = merge(head2.nest, head1)
     
     return current
+'''
+# This is the function which accepts 2 linked list and returns the head of the new merged list
+# Loop approach
+def merge(llist1, llist2):
+    cur1 = llist1.head
+    cur2 = llist2.head
+    mergerd = linked_list()
     
+    if not cur1:
+        return llist2.head
+    if not cur2:
+        return llist1.head
+    
+    if cur1 and cur2:
+        if cur2.data<=cur1.data:
+            mergerd.append(cur2.data)
+            cur2 = cur2.nest
+        else:
+            mergerd.append(cur1.data)
+            cur1 = cur1.nest
+        
+    
+    # this loop should continue untill either of the 2 gets fully esplored
+    while cur2 and cur1:
+        if cur1.data<=cur2.data:
+            mergerd.append(cur1.data)
+            cur1 = cur1.nest
+        else:
+            mergerd.append(cur2.data)
+            cur2 = cur2.nest
+    
+    # After the previous loop 1 of the list gets traversed completely, so we need to traverse the remaining list
+    
+    while cur1:
+        mergerd.append(cur1.data)
+        cur1 = cur1.nest
+    while cur2:
+        mergerd.append(cur2.data)
+        cur2 = cur2.nest
+    
+    return mergerd.head
+'''   
 
 llist1 = linked_list()
 llist1.append('1')
@@ -64,5 +105,6 @@ llist2.append('6')
 llist2.append('8')
 
 llist3 = linked_list()
-llist3.head = merge(llist1.head, llist2.head)  
+llist3.head = merge(llist1.head, llist2.head)
+# llist3.head = merge(llist1, llist2) Use this if we are calling the loop approach
 llist3.printlist()
