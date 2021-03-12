46ec3d2c1d155697687d9eb19ea155d2db803777
Sayan Brahma <47536918+sbrahma0@users.noreply.github.com>
2021-03-11 23:17:12 -0600

    Update minSwap_consecutive.py

--- minSwap_consecutive.py
+++ minSwap_consecutive.py
@@ -3,6 +3,7 @@
 # This is diff from adjacent swaps and if the numbers are not consecutive
 
 def min_swaps(arr):
+    arr = [0]+arr
     hash = {element:index for index,element in enumerate(arr)} 
     
     total_swap_count=0
@@ -23,5 +24,5 @@ def min_swaps(arr):
     
     return total_swap_count
 
-a = [0,2,3,4,1,6,5]
+a = [2,3,4,1,6,5]
 print(min_swaps(a))
